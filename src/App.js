ee02249a2244196f132d1132141b448747a9c10e
Shubham <shubhamp1251@gmail.com>
2022-04-08 22:10:50 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,18 +5,35 @@ import  Contact  from './components/Contact';
 import About from './components/About';
 import Skills from './components/Skills';
 import Header from './components/Header';
-import {useState } from 'react';
+import {useEffect, useState } from 'react';
 
 function App() {
   
   const [theme, setTheme]  = useState('light');
 
 
+  useEffect(() => {
+
+    if (sessionStorage.getItem('theme-color')===null) {
+      setTheme('light');
+    }
+    else{
+      const state = JSON.parse(sessionStorage.getItem('theme-color'));
+       setTheme(state);
+    }
+    
+  }, [])
+
   const changeTheme = ()=>{
     
-    theme === "light" ? setTheme("dark") : setTheme('light')
-
+    theme === "light" ? setTheme("dark") : setTheme('light');
+    
+    theme === "light" ? sessionStorage.setItem('theme-color',JSON.stringify('dark')) : sessionStorage.setItem('theme-color',JSON.stringify('light'));
   }
+
+ 
+
+  
    
 
   return (
